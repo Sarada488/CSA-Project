df5cc0794047f69d151df379eb2a9befd5f422d5
Sarada488 <saradamatia@chatham-nj.org>
2026-06-02 08:43:31 -0400

    Update scenarios.js

--- data/scenarios.js
+++ data/scenarios.js
@@ -2,23 +2,67 @@ const scenarios = [
 
 {
   name:"Phishing Campaign",
-
-  description:
-  "Employees are receiving malicious emails."
+  description:"Employees are receiving malicious emails."
 },
 
 {
   name:"Ransomware",
-
-  description:
-  "File servers are being encrypted."
+  description:"File servers are being encrypted."
 },
 
 {
   name:"Insider Threat",
+  description:"An employee is exfiltrating data."
+},
 
-  description:
-  "An employee is exfiltrating data."
+{
+  name:"Business Email Compromise",
+  description:"Executive accounts are sending fraudulent payment requests."
+},
+
+{
+  name:"Supply Chain Compromise",
+  description:"A trusted vendor update contains malicious code."
+},
+
+{
+  name:"DDoS Attack",
+  description:"External services are being overwhelmed by traffic."
+},
+
+{
+  name:"Privilege Escalation",
+  description:"An attacker is attempting to gain administrator access."
+},
+
+{
+  name:"Data Exfiltration",
+  description:"Sensitive files are being transferred externally."
+},
+
+{
+  name:"Cloud Misconfiguration",
+  description:"Public storage buckets are exposing sensitive data."
+},
+
+{
+  name:"Credential Stuffing",
+  description:"Attackers are testing leaked passwords."
+},
+
+{
+  name:"Malicious USB",
+  description:"An employee connected an unknown USB device."
+},
+
+{
+  name:"Insider Sabotage",
+  description:"A privileged employee is deleting assets."
+},
+
+{
+  name:"Web Application Attack",
+  description:"Attackers are targeting a public-facing application."
 }
 
 ];
