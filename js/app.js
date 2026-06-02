d9f6e3bd0e0763d44154bc8a7f875e8fa0cfbbbb
Sarada488 <saradamatia@chatham-nj.org>
2026-06-02 09:03:37 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
+console.log("app starting");
+
 loadPlayer();
+
+console.log("player loaded");
+
 updateUI();
+
+console.log("ui updated");
+
 startScenario();
 
+console.log("scenario started");
+
+setInterval(
+  generateAlert,
+  3000
+);
+
+console.log("alert timer started");
 document
 .getElementById(
   "terminalInput"
@@ -23,10 +39,3 @@ document
   }
 );
 
-setInterval(
-
-  generateAlert,
-
-  3000
-
-);
