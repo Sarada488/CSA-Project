6d92b38275d20ec619f38b48df27d01e8a87bae6
Sarada488 <saradamatia@chatham-nj.org>
2026-06-02 07:13:51 -0400

    Create scenarios.js

---
+++ data/scenarios.js
@@ -0,0 +1,24 @@
+const scenarios = [
+
+{
+  name:"Phishing Campaign",
+
+  description:
+  "Employees are receiving malicious emails."
+},
+
+{
+  name:"Ransomware",
+
+  description:
+  "File servers are being encrypted."
+},
+
+{
+  name:"Insider Threat",
+
+  description:
+  "An employee is exfiltrating data."
+}
+
+];
