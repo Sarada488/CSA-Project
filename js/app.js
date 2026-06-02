9d655c4b1df3f8c5a670e396db64c2353b9f4f9f
Sarada488 <saradamatia@chatham-nj.org>
2026-06-02 07:16:54 -0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,30 @@
+loadPlayer();
+
+document
+.getElementById(
+  "terminalInput"
+)
+.addEventListener(
+  "keydown",
+  function(e){
+
+    if(e.key === "Enter"){
+
+      runCommand(
+        this.value
+      );
+
+      this.value = "";
+
+    }
+
+  }
+);
+
+setInterval(
+
+  generateAlert,
+
+  3000
+
+);
