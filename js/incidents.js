b32c6cf9106063e02cc2115e7957698cec4d873d
Sarada488 <saradamatia@chatham-nj.org>
2026-06-04 10:41:34 -0400

    Update incidents.js

--- js/incidents.js
+++ js/incidents.js
@@ -15,3 +15,34 @@ function createIncident(alert){
   });
 
 }
+
+function renderIncidents(){
+
+  const container =
+    document.getElementById(
+      "incidentContainer"
+    );
+
+  if(!container) return;
+
+  container.innerHTML = "";
+
+  incidents.forEach(incident => {
+
+    const div =
+      document.createElement("div");
+
+    div.className =
+      "bg-slate-800 p-3 rounded mb-2";
+
+    div.innerHTML = `
+      ${incident.host}
+      (${incident.severity})
+      - ${incident.status}
+    `;
+
+    container.appendChild(div);
+
+  });
+
+}
