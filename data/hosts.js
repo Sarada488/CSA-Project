7b6657b0270304f2807943244e9c8e1d73bd7fda
Sarada488 <saradamatia@chatham-nj.org>
2026-06-04 10:37:33 -0400

    Create hosts.js

---
+++ data/hosts.js
@@ -0,0 +1,18 @@
+const networkHosts = {
+
+  "DC-01":{
+    compromised:false,
+    isolated:false
+  },
+
+  "FILESERVER":{
+    compromised:false,
+    isolated:false
+  },
+
+  "HR-PC":{
+    compromised:false,
+    isolated:false
+  }
+
+};
